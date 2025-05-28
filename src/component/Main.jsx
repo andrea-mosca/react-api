44acf581d8b755f9b30caf76df9c32318fb5a75b
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-28 15:28:15 +0200

    milestone2/3: create a card for each actor

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,14 +1,48 @@
 import axios from "axios";
-
+import { useState, useEffect } from "react";
 const apiBaseUrl = "https://lanciweb.github.io/demo/api";
 
 export default function Main() {
-  axios.get(`${apiBaseUrl}/actors/`).then((res) => {
-    console.log(res.data);
-  });
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${apiBaseUrl}/actors/`).then((res) => {
+      setResults(res.data);
+    });
+  }, []);
+
   return (
     <main>
-      <div className="container"></div>
+      <div className="container">
+        <div className="row row-cols-4">
+          {results.map((result, id) => (
+            <div className="col" key={result.id}>
+              <div className="card h-100">
+                <img
+                  src={result.image}
+                  alt={result.name}
+                  className="card-img-top w-100"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{result.name}</h5>
+                  <p className="card-text">{result.biography}</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">
+                    Anno di nascita: {result.birth_year}
+                  </li>
+                  <li className="list-group-item">
+                    Nazionalità: {result.nationality}
+                  </li>
+                  <li className="list-group-item">
+                    Riconoscimenti: {result.awards}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
