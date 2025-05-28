3f0afd17bab0d7e31284a5c8952f0ed1ad3eeb19
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-28 14:53:24 +0200

    milestone1: print in console the lists of actors

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,5 +1,14 @@
 import axios from "axios";
 
+const apiBaseUrl = "https://lanciweb.github.io/demo/api";
+
 export default function Main() {
-  return;
+  axios.get(`${apiBaseUrl}/actors/`).then((res) => {
+    console.log(res.data);
+  });
+  return (
+    <main>
+      <div className="container"></div>
+    </main>
+  );
 }
